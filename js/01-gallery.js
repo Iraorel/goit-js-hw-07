f31e94b2e484afef7f0bdb8ead4ddb43d68a3533
Iraorel <79316590+Iraorel@users.noreply.github.com>
2021-10-27 21:40:40 +0300

    script gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,9 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const conteiner = document.querySelector('.gallery');
+conteiner.addEventListener('click', onClick);
+function onClick(evt){
+    console.log(evt.target);
+}
