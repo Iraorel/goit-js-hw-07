e5d73a4b6824714953b4d0001b499b939369fde9
Iraorel <79316590+Iraorel@users.noreply.github.com>
2021-10-29 21:37:32 +0300

    gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,10 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
 const conteiner = document.querySelector('.gallery');
-conteiner.addEventListener('click', onClick);
-function onClick(evt){
-    console.log(evt.target);
+//conteiner.addEventListener('click', onClick);
+//function onClick(evt){
+   // console.log(evt.target);
+//}
+const imgGallery = createGalleryItems(galleryItems);
+
+conteiner.insertAdjacentHTML('beforeend', imgGallery);
+function createGalleryItems (galleryItems){
+
+    return galleryItems.map( item  =>{
+        return 
+        `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+})
+.join('');
+
 }
